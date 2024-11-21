15d79a62203daf81b5f4c44ced63af69712590c8
Chia-kuo, Chen <tear4701@gmail.com>
2024-11-21 16:08:26 +0800

    Initial Commit

---
+++ PersonalityTest/PersonalityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalityTestApp.swift
+//  PersonalityTest
+//
+//  Created by 陳嘉國 on 2024/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PersonalityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
